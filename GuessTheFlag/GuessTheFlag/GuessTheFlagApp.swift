d6003d6835cb99a0121c162a9e8e8d63cf57844d
iamalexh <67149386+iamalexh@users.noreply.github.com>
2020-11-03 13:45:58 +0000

    Add files via upload

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Alex on 03/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
